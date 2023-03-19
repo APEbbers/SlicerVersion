89e2bc19daeabd5e05972ad64adb199a0f78a107
APEbbers <paul.ebbers@gmail.com>
2023-03-19 10:35:55 +0100

    changed name to SlicerVersion.exe

---
+++ SlicerVersion.py
@@ -0,0 +1,16 @@
+import sys
+
+sourcefile = sys.argv[1]
+
+with open(sourcefile, mode='r', encoding='UTF-8') as fopen:
+    data = fopen.readline()
+    print(data)
+    objNewLine = data
+    objNewLine = objNewLine.split("by")[1]
+    objNewLine = objNewLine.split("on")[0]
+    print(objNewLine)
+
+    seq = "; slicer_version = " + objNewLine[1:] + "\n;"
+
+with open(sourcefile, mode='a', encoding='UTF-8') as fopen:
+    fopen.write(seq)
--- dist/test.bat
+++ dist/test.bat
@@ -1,2 +1,2 @@
-"SlicerVersion - Prusa.exe" "Gary_0.12mm_PETG_6h35m.gcode"
+"SlicerVersion.exe" "Gary_0.12mm_PETG_6h35m.gcode"
 pause

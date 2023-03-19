0685a1366b976a4b853aeb743c19ea56f03cb2db
APEbbers <paul.ebbers@gmail.com>
2023-03-19 09:49:40 +0100

    Fixed .exe

--- SlicerVersion - Prusa.py
+++ SlicerVersion - Prusa.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 import sys
 
 sourcefile = sys.argv[1]
@@ -12,8 +10,7 @@ with open(sourcefile, mode='r', encoding='UTF-8') as fopen:
     objNewLine = objNewLine.split("on")[0]
     print(objNewLine)
 
-    seq = "; slicer_version = "+ objNewLine[1:] + "\n;"
-    
-with open(sourcefile, mode='a', encoding='UTF-8') as fopen:        
+    seq = "; slicer_version = " + objNewLine[1:] + "\n;"
+
+with open(sourcefile, mode='a', encoding='UTF-8') as fopen:
     fopen.write(seq)
-    
